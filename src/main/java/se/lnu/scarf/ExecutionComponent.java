8150762b30fe3f692fb9d55f10667595da105d11
Samuele Giussani <samuele.giussani@lnu.se>
2025-12-18 17:06:59 +0100

    Execution changed to reflected Method

--- src/main/java/se/lnu/scarf/ExecutionComponent.java
+++ src/main/java/se/lnu/scarf/ExecutionComponent.java
@@ -4,8 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 public class ExecutionComponent {
@@ -14,22 +21,29 @@ public class ExecutionComponent {
     public void execute(Path baseFolder, String mainClassQualifiedName) throws Exception {
         logger.info("Starting code execution...");
         logger.info("Targeting {}", mainClassQualifiedName);
-        Path classesFolder = baseFolder.resolve("target/classes");
+        List<URL> urls = new ArrayList<>();
+        Path targetFolder = baseFolder.resolve("target/classes");
         Path libFolder = baseFolder.resolve("lib");
-        String libJars = libFolder + File.separator + "*";
-        String classpath = classesFolder + File.pathSeparator + libJars;
-        logger.info(classpath);
-
-        ProcessBuilder processBuilder = new ProcessBuilder(
-                "java",
-                "-Xmx2g",
-                "-cp", classpath,
-                mainClassQualifiedName
-        );
-        processBuilder.directory(baseFolder.toFile());
-        processBuilder.inheritIO();
-        Process process = processBuilder.start();
-        logger.info("Executing simulation...");
-        process.waitFor();
+        urls.add(targetFolder.toUri().toURL());
+        try (Stream<Path> stream = Files.list(libFolder)) {
+            urls.addAll(stream.filter(p -> p.toString().endsWith(".jar"))
+                    .map(p -> {
+                        try { return p.toUri().toURL(); } catch (MalformedURLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).toList());
+        }
+        try (URLClassLoader classLoader = new URLClassLoader(urls.toArray(new URL[0]), null)) {
+            Thread.currentThread().setContextClassLoader(classLoader);
+            Class<?> mainClass = Class.forName(mainClassQualifiedName, true, classLoader);
+            Method mainMethod = mainClass.getMethod("main", String[].class);
+            mainMethod.setAccessible(true);
+            String[] arguments = {};
+            logger.info("Executing main method...");
+            mainMethod.invoke(null, (Object) arguments);
+        } finally {
+            Thread.currentThread().setContextClassLoader(null);
+            System.gc();
+        }
     }
 }
